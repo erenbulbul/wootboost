1719ce2912e0dd5d9aab2f9f9d02de1e968b1076
karabae <buhan.karahan@gmail.com>
2017-07-27 21:24:34 +0300

    Make some performance improvements and fix bugs. 1.0.0 here!

--- wootboost.user.js
+++ wootboost.user.js
@@ -1,18 +1,18 @@
 // ==UserScript==
 // @name         wootboot
 // @namespace    https://wb.karabae.com/
-// @version      1.0
+// @version      1.0.0
 // @description  The script allows you to boost your level via woot on plug.dj!
 // @author       karabae
 // @include     https://plug.dj*
 // ==/UserScript==
 
+//jshint esversion: 6
+
 function checkThenWoot() {
-    if (!getElementById("woot").classList.contains('selected'))
-        getElementById("woot").click();
+    let wootElement = document.getElementById("woot");
+    if (!wootElement.classList.contains('selected'))
+        wootElement.click();
 }
 
 setInterval(checkThenWoot, 10000);
-
-
-main();
