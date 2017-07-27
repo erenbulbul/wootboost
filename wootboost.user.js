dbd60784084891bd1f18267c461e356a00c92680
karabae <buhan.karahan@gmail.com>
2017-07-27 21:36:02 +0300

    Make script name correct and add auto-update thing

--- wootboost.user.js
+++ wootboost.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
-// @name         wootboot
+// @name         wootboost
 // @namespace    https://wb.karabae.com/
 // @version      1.0.0
 // @description  The script allows you to boost your level via woot on plug.dj!
 // @author       karabae
 // @include     https://plug.dj*
+// @downloadURL https://github.com/karabae/wootboost/raw/master/wootboost.user.js
+// @updateURL https://github.com/karabae/wootboost/raw/master/wootboost.user.js
 // ==/UserScript==
 
 //jshint esversion: 6
