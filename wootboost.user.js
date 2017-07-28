7a7ed8913c3d0405c807a249372700de22e41d12
karabae <buhan.karahan@gmail.com>
2017-07-28 20:33:16 +0300

    Some URL changes.

--- wootboost.user.js
+++ wootboost.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         wootboost
-// @namespace    https://wb.karabae.com/
+// @namespace    https://wb.karahanbuhan.com/
 // @version      1.0.0
 // @description  The script allows you to boost your level via woot on plug.dj!
-// @author       karabae
+// @author       H Karahan Buhan
 // @include     https://plug.dj*
-// @downloadURL https://github.com/karabae/wootboost/raw/master/wootboost.user.js
-// @updateURL https://github.com/karabae/wootboost/raw/master/wootboost.user.js
+// @downloadURL https://github.com/karahanbuhan/wootboost/raw/master/wootboost.user.js
+// @updateURL https://github.com/karahanbuhan/wootboost/raw/master/wootboost.user.js
 // ==/UserScript==
 
 //jshint esversion: 6
